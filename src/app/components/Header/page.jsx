bbfc8d2a3bfc7b8496cc44890c3dbf7dde1c69fb
gunjanvshah <gunjanvshah@gmail.com>
2023-12-08 17:40:46 +0530

    new branch design

--- src/app/components/Header/page.jsx
+++ src/app/components/Header/page.jsx
@@ -21,15 +21,7 @@ const Header = () => {
           id="navbar-multi-level"
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0">
-            <li>
-              <Link
-                href="/"
-                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
-                aria-current="page"
-              >
-                Home
-              </Link>
-            </li>
+           
             <li>
               <Link
                 href="/"
