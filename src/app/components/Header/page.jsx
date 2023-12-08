c55af6ebaba640bd88fcfc3715a5c9bbbdb62bbd
gunjanvshah <gunjanvshah@gmail.com>
2023-12-08 17:35:52 +0530

    test

--- src/app/components/Header/page.jsx
+++ src/app/components/Header/page.jsx
@@ -32,6 +32,15 @@ const Header = () => {
             </li>
             <li>
               <Link
+                href="/"
+                className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
+                aria-current="page"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
                 href="/treatments"
                 className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0"
               >
