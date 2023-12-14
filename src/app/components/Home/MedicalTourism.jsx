1e46e3ddfa793d083847a058c9a390b2c309d143
gunjanvshah <gunjanvshah@gmail.com>
2023-12-14 14:30:11 +0530

    medigo_form

--- src/app/components/Home/MedicalTourism.jsx
+++ src/app/components/Home/MedicalTourism.jsx
@@ -25,16 +25,7 @@ Medical tourism is a rapidly growing global industry, and India has emerged as o
         <label for="floating_last_name" className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Last name</label>
     </div>
    
-    <div className="relative z-0 w-full mb-5 group">
-  <select id="countries" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
-
-    <option>United States</option>
-    <option>Canada</option>
-    <option>France</option>
-    <option>Germany</option>
-  </select>
-
-</div>
+  
     <div className="relative z-0  mb-5 group flex w-full space-x-4">
     <select id="countries" className="block py-2.5 px-0  text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer">
 
